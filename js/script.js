2ba769ad2825a2032fc5ddb53da50101441d848c
Dionatan Rodrigues <48455774+Dionatan2019Rodrigues@users.noreply.github.com>
2023-04-25 15:06:09 -0300

    finish

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-const input = document.querySelector("input");
+const input = document.getElementById("input-tema");
 const body = document.querySelector("body");
+const main = document.querySelector("main");
 
 const toggleThemeMode = () => {
+
     body.classList.toggle("dark");
+    main.classList.toggle("dark-mode");
 };
 
 input.onchange = toggleThemeMode;
